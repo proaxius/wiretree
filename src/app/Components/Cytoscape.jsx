efb1f20e05082c32458c661140e92dc0f11d173e
anurag <proaxius@gmail.com>
2024-03-03 15:39:06 +0530

    tested some things

--- src/app/Components/Cytoscape.jsx
+++ src/app/Components/Cytoscape.jsx
@@ -1,8 +1,11 @@
 'use client'
-import React, { useEffect,useRef } from 'react'
+import React, { useEffect, useState,useRef } from 'react'
 import cytoscape from 'cytoscape'
+
 export default function Cytoscape() {
     const cyRef = useRef(null)
+    let [ nodeId ,setNodeId] = useState(0)
+    let [ nodeLabel ,setNodelabel] = useState('label')
 
     useEffect( 
       ()=>{
@@ -10,17 +13,14 @@ export default function Cytoscape() {
             const cy = cytoscape({
                 container: cyRef.current,
 
-                elements: [
-                    { data: { id: 'a' } },
-                    { data: { id: 'b' } },
-                    { data: { id: 'ab', source: 'a', target: 'b' } }
-                  ],
+            
                   style: [
                     {
                       selector: 'node',
                       style: {
                         'background-color': '#666',
-                        'label': 'data(id)'
+                        'label': 'data(id)',
+                        'color':'rgb(38,217,59)'
                       }
                     },
                     {
@@ -34,15 +34,43 @@ export default function Cytoscape() {
                     }
                   ]
                 });
-            cy.on('clieck','node',(event)=>{
-                console.log('Node clicked')
-            })
+                cy.on('click',(event)=>{
+                let clientX = event.originalEvent.clientX
+                let clientY= event.originalEvent.clientY
+                var canvasCords = cy.forceRender().container().getBoundingClientRect()
+                var cyPosition = {
+                  x : clientX - canvasCords.left,
+                  y : clientY - canvasCords.top,
+                };
+
+                var zoom = cy.zoom();
+                var pan = cy.pan();
+                cyPosition.x = ( cyPosition.x - pan.x) / zoom;
+                cyPosition.y = ( cyPosition.y - pan.y) / zoom;
+                console.log('cyPosition', cyPosition);
+                cy.add( ()=>{
+                  let newNodeId = nodeId +1
+                  setNodeId(newNodeId)
+
+                },{
+                  selector: 'node',
+                  group: 'nodes',
+                  data:{id:nodeId,
+                  label : 'Label'
+                },
+                  position:{
+                    x:cyPosition.x,
+                    y:cyPosition.y
+                  }
+                })
+                })
                 return () => {
                   cy.destroy();
                 
                 };
-        },[])
+        },
+        [])
   return (
-    <div className='h-screen' ref={cyRef}></div>
+    <div className='h-screen block' id="canvas" ref={cyRef}></div>
   )
 }
